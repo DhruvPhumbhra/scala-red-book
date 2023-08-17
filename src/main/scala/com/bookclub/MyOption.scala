85b06d24467458fe7b2305322ce798cae6d6eb48
Dhruv Phumbhra <dphumbhra@dwolla.com>
2023-08-17 16:06:59 -0500

    option exercise; map and getOrElse

--- src/main/scala/com/bookclub/MyOption.scala
+++ src/main/scala/com/bookclub/MyOption.scala
@@ -1,16 +1,46 @@
 package com.bookclub
 
-trait MyOption[+A]
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration.Inf
+import scala.concurrent.{Await, Future}
+
+trait MyOption[+A] {
+  def map[B](f: A => B): MyOption[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(value) => value
+  }
+
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] = ???
+
+  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = ???
+
+  def filter(f: A => Boolean): MyOption[A] = ???
+}
+
 case class Some[+A](get: A) extends MyOption[A]
 case object None extends MyOption[Nothing]
 
+object TestOptionFunctionsMap extends App {
+  println(Some(5).map(_ + 1)) // this.map(_ + 1)
+  println(Some(5).map(x => x + 1).map(_ + 2))
+}
 
-
+object TestOptionFunctions extends App {
+//  Some(5).map(_ + 1)
+//  println(Some(5).getOrElse(Await.result(Future.successful(Thread.sleep(10000)).map(_ => 4), Inf)))
+  println(None.getOrElse(Await.result(Future.successful(Thread.sleep(10000)).map(_ => 4), Inf)))
+//  println(None.getOrElse(Await.result(Future.successful(Thread.sleep(1000)).map(_ => 4), Inf)))
+}
 
 object TestRT extends App {
 
   def failingFn(i: Int): Int = {
-    val y: Int = throw new Exception("fail!")
+    lazy val y: Int = throw new Exception("fail!")
 
     try {
       val x = 42 + 5
@@ -31,7 +61,8 @@ object TestRT extends App {
     }
   }
 
-  println(failingFn2(12))
+  println(failingFn(12))
+//  println(failingFn2(12))
 }
 
 object AltsToExceptions extends App {
@@ -43,6 +74,10 @@ object AltsToExceptions extends App {
   def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double =
     if (xs.isEmpty) onEmpty
     else xs.sum / xs.length
+
+  def mean_2(xs: IndexedSeq[Double]): MyOption[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
 }
 
 
